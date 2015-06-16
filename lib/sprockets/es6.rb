00b42c9744ef95eac340b34afb989bd87375fef1
Ryan McKay-Fleming <ryan@chalk.com>
2015-06-16 16:44:59 -0400

    Force commonjs modules

--- lib/sprockets/es6.rb
+++ lib/sprockets/es6.rb
@@ -40,6 +40,6 @@ module Sprockets
 
   append_path Babel::Transpiler.source_path
   register_mime_type 'text/ecmascript-6', extensions: ['.es6'], charset: :unicode
-  register_transformer 'text/ecmascript-6', 'application/javascript', ES6
+  register_transformer 'text/ecmascript-6', 'application/javascript', Sprockets::ES6.new('modules' => 'common')
   register_preprocessor 'text/ecmascript-6', DirectiveProcessor
 end
